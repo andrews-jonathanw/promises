a43198779bc690ad2770583ec81078285b91b7b4
Jonathan Andrews <andrews.jonathanw@gmail.com>
2023-07-24 11:26:03 -0500

    Complete bare_minimum/callbackReview.js exercises.

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,13 +6,27 @@ var fs = require('fs');
 var request = require('needle');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
+var pluckFirstLineFromFile = function (filePath, cb) {
   // TODO
+  fs.readFile(filePath, 'utf8', function (error, data) {
+    if (error) {
+      cb(error);
+    } else {
+      cb(null, data.split('\n')[0]);
+    }
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
+var getStatusCode = function (url, cb) {
   // TODO
+  request('get', url, (error, response) => {
+    if (error) {
+      cb(error);
+    } else {
+      cb(null, response.statusCode);
+    }
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
